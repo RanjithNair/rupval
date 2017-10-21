40bca2c6251b6e63a9845c43004dbbcbeeac074f
RanjithNair <ranjith2112@gmail.com>
2017-10-20 22:33:14 -0500

    Using puppeteer heroku buildpack

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer')
 const fetch = require('node-fetch')
 
 async function getRates () {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']})
   const page = await browser.newPage()
   // Remitly
   await page.goto('https://www.remitly.com/us/en/india')
